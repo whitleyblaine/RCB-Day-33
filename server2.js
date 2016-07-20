8fad8e81df67817434d98403deee9fb7bc87a5e6
Whitley Blaine <whitleyblaine@gmail.com>
2016-07-19 20:56:16 -0400

    Multiple-page site running

--- server2.js
+++ server2.js
@@ -1,24 +1,40 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
   if (req.url === '/') {
-    res.writeHead(200, {
-      'Content-Type' : 'text/html'
-    });
-    res.end('<h1>Welcome to my home page!</h1>');
+    fs.readFile('index.html', function(err, data) {
+      // Tell the browser the string called "data" is HTML, because it wouldn't inherently know. It just imports a STRING, the data from your file
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      // Return the data, formatting as HTML
+      res.end(data);
+    })
   }
-  else if (req.url === '/about') {
-    res.writeHead(200, {
-      'Content-Type' : 'text/html'
-    });
-    res.end('<h1>Welcome to my about page!</h1>');
+  else if (req.url === '/movies') {
+    fs.readFile('movies.html', function(err, data) {
+      // Tell the browser the string called "data" is HTML, because it wouldn't inherently know. It just imports a STRING, the data from your file
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      // Return the data, formatting as HTML
+      res.end(data);
+    })
   }
-  else if (req.url === '/contact') {
-    res.writeHead(200, {
-      'Content-Type' : 'text/html'
-    });
-    res.end('<h1>Welcome to my contact page!</h1>');
-  } else {
+  else if (req.url === '/foods') {
+    fs.readFile('foods.html', function(err, data) {
+      // Tell the browser the string called "data" is HTML, because it wouldn't inherently know. It just imports a STRING, the data from your file
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      // Return the data, formatting as HTML
+      res.end(data);
+    })
+  }
+  else if (req.url === '/frameworks') {
+    fs.readFile('frameworks.html', function(err, data) {
+      // Tell the browser the string called "data" is HTML, because it wouldn't inherently know. It just imports a STRING, the data from your file
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      // Return the data, formatting as HTML
+      res.end(data);
+    })
+  }
+  else {
     res.writeHead(404, {
       'Content-Type' : 'text/html'
     });
